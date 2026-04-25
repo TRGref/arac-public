dd3fc32072535c47b924ffe2a113354bf70a12e1
Gref <mabzkrt1905@gmail.com>
2026-04-25 23:29:25 +0300

    Surum v1.0.138+138 (web push)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -79,16 +79,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "b1882c7f702161983333f856bc4b88d8
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5b73a84f50dbdad9ad5347663a232102",
+"flutter_bootstrap.js": "c448b87be6416e5a5a5ad65efcd833b3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b62f803b600e7a317233bd682cbe0553",
-"/": "b62f803b600e7a317233bd682cbe0553",
+"index.html": "763fcd5f5ed657a41d50fbbb5c1f00ed",
+"/": "763fcd5f5ed657a41d50fbbb5c1f00ed",
 "main.dart.js": "727153950edf132b27136d453942657f",
 "manifest.json": "cfbea99b5b4a1d7e2b4e525210c8d6d8",
-"version.json": "5d07cb1a9c59dec0dd059aac34bb3e79",
+"version.json": "26c30df8b98bca3ab32606b1db13a5b5",
 "web.zip": "8acf86b8fad1d013396418d4d0940ff7"};
 // The application shell files that are downloaded before a service worker can
 // start.
